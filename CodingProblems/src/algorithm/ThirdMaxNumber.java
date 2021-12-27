3544c62e845c6bf63f19ca589d8b9197e9ba9147
KsumiKim <ksumiKim@gmail.com>
2021-12-27 20:54:37 +0900

    return third max number of given array

--- CodingProblems/src/algorithm/ThirdMaxNumber.java
+++ CodingProblems/src/algorithm/ThirdMaxNumber.java
@@ -1,13 +1,17 @@
 package algorithm;
 
 import java.util.HashSet;
-import java.util.TreeSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ThirdMaxNumber {
 
 	public static void main(String[] args) {
 		int[] num = {1, 2, 3, 2};
-		System.out.println(getThirdMaxNumber(num));
+		int[] num2 = {1, 2};
+		int[] num3 = {3, 2, 1};
+		System.out.println(thirdMax(num));
 	}
 	
 	public static int getThirdMaxNumber(int[] numbers) {
@@ -41,51 +45,14 @@ public class ThirdMaxNumber {
 		return max;
 	}
 
-	public static int getThirdMax(int[] numbers) {
-		// TreeSet: store values in ascending order
-		TreeSet<Integer> set = new TreeSet<>();
-		
-		for (int n : numbers) {
-			set.add(n);
-		}
-		
-		// if the given array has less than three numbers 
-		// return the max number
-		if (set.size() < 3)
-			return set.last();
-		
-		// remove first and second max numbers
-		set.remove(set.last());
-		set.remove(set.last());
-		
-		return set.last();
-	}
-
-	public int getThirdMaxValue(int[] num) {
-		Integer max1 = null;
-		Integer max2 = null;
-		Integer max3 = null;
-		
-		for (Integer n : num) {
-			
-			// when there are duplicate values in array continue the loop
-			if (n.equals(max1) || n.equals(max2) || n.equals(max3))
-				continue;
-			
-			if (max1 == null || n > max1) {
-				max3 = max2;
-				max2 = max1;
-				max1 = n;
-			} else if (max2 == null || n > max2) {
-				max2 = max1;
-				max2 = n;
-			} else if (max3 == null || n > max3) {
-				max3 = n;
-			}
-		}
-		// max3 being null means there are less than 3 values in the given array
-		// in that case, return the max value (not third max)
-		return max3 == null ? max1 : max3;
-	}
-	
+    public static int thirdMax(int[] nums) {
+    	
+    	Set<Integer> set = new HashSet<>();
+    	for (int n : nums) {
+    		set.add(n);
+    	}
+    	
+    	List<Integer> sortedUniqueList = set.stream().sorted().collect(Collectors.toList());
+    	return sortedUniqueList.size() > 3 ? sortedUniqueList.get(sortedUniqueList.size() - 3) : ( sortedUniqueList.size() == 3 ? sortedUniqueList.get(0) : sortedUniqueList.get(sortedUniqueList.size() - 1) );
+    }
 }
