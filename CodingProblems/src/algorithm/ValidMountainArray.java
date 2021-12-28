64f18520fe70f2bf8354486b7d1b8d3bc89aebb5
KsumiKim <ksumiKim@gmail.com>
2021-12-28 21:54:40 +0900

    return if given array is a valid mountain array

--- CodingProblems/src/algorithm/ValidMountainArray.java
+++ CodingProblems/src/algorithm/ValidMountainArray.java
@@ -4,8 +4,12 @@ public class ValidMountainArray {
 
 	public static void main(String[] args) {
 		int[] A = {0, 3, 2, 1};
+		int[] A2 = {2, 1};
+		int[] A3 = {3, 5, 5};
+		int[] A4 = {9,8,7,6,5,4,3,2,1,0};
+		int[] A5 = {0,1,2,3,4,5,6,7,8,9};
 		ValidMountainArray mountArray = new ValidMountainArray();
-		boolean res = mountArray.validMountainArray(A);
+		boolean res = mountArray.validMountainArray2(A);
 		System.out.println(res);
 	}
 	
@@ -37,5 +41,38 @@ public class ValidMountainArray {
     	}
     	
     	return true;
+    }
+    
+    public boolean validMountainArray2(int[] arr) {
+    	
+    	if (arr.length < 3) {
+    		return false;
+    	}
+    	
+    	int prev = arr[0];
+    	boolean increasing = true;
+    	
+    	for (int i = 1; i < arr.length; i++) {
+    		
+    		if (arr[i] == prev) {
+    			return false;
+    		}
+    		
+    		if (i == 1 && arr[i] < prev) {
+    			return false;
+    		}
+    		
+    		if (increasing && arr[i] < prev) {
+    			increasing = false;
+    		}
+    		
+    		if (!increasing && arr[i] > prev) {
+    			return false;
+    		}
+    		
+    		prev = arr[i];
+    	}
+    	
+    	return !increasing;
     }
 }
