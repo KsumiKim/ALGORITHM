041f867b19cd02f62b3eb1042e86a11875e4134f
KsumiKim <ksumiKim@gmail.com>
2022-01-11 21:13:53 +0900

    return if given string s follows the pattern

--- CodingProblems/src/algorithm/WordPattern.java
+++ CodingProblems/src/algorithm/WordPattern.java
@@ -9,91 +9,33 @@ public class WordPattern {
 		String pattern = "abba";
 		String str = "dog cat dog dog";
 		
-		// System.out.println(hasSamePattern(pattern, str));
+		String pattern2 = "abba";
+		String str2 = "dog cat cat fish";
+
+		String pattern3 = "aaaa";
+		String str3 = "dog cat cat dog";
+
+		String pattern4 = "abba";
+		String str4 = "dog cat cat dog";
 		
-		System.out.println(samePattern(pattern, str));
+		System.out.println(wordPattern(pattern4, str4));
 	}
 	
-	public static boolean hasSamePattern(String pattern, String str) {
-		
-		// put pattern into map and give each letter an index
-		HashMap<String, Integer> pMap = new HashMap<String, Integer>();
-		
-		for (int i = 0; i < pattern.length(); i++) {
-			
-			if (!pMap.containsKey(pattern.charAt(i) + ""))
-				pMap.put(pattern.charAt(i) + "", i);
-		}
-		
-		// put indices of pattern into int array 
-		int[] pArray = new int[pattern.length()];
-		
-		for (int i = 0; i < pattern.length(); i++) {
-			pArray[i] = pMap.get(pattern.charAt(i) + "");
-		}
-		
-		// put each word into Map after splitting the given string by space: " "
-		String[] strArray = str.split(" ");
-		HashMap<String, Integer> sMap = new HashMap<String, Integer>();
-		
-		for (int i = 0; i < strArray.length; i++) {
-
-			if (!sMap.containsKey(strArray[i]))
-				sMap.put(strArray[i], i);
-		}
-		
-		// put indices of each word into int array 
-		int[] sArray = new int[strArray.length];
-		
-		for (int i = 0; i < strArray.length; i++) {
-			sArray[i] = sMap.get(strArray[i]);
-		}
-		
-		
-		// check if two arrays are the same 
-		for (int i = 0; i < pArray.length; i++) {
-			
-			if (pArray[i] != sArray[i])
-				return false;
-		}
-		
-		return true;
-	}
-	
-	public static boolean wordPattern(String pattern, String str) {
-	    String[] words = str.split(" ");
-
-	    if (words.length != pattern.length())
-	        return false;
-	    
-	    // declare Map without type
-	    Map index = new HashMap();
-	 
-	    for (Integer i = 0; i < words.length; ++i) {
-	    	
-	    	// map.put returns the previous value of associated key 
-	    	if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
-	            return false;
-	    }
-	    
-	    return true;
-	}
-
-	public static boolean samePattern(String pattern, String str) {
-		String[] strArr = str.split(" ");
+	public static boolean wordPattern(String pattern, String s) {
 		Map<Character, String> map = new HashMap<>();
+		String[] words = s.split(" ");
 		
 		for (int i = 0; i < pattern.length(); i++) {
+			char c = pattern.charAt(i);
 			
-			if (map.containsKey(pattern.charAt(i))) {
-				if (!map.get(pattern.charAt(i)).equals(strArr[i])) {
+			if (map.containsKey(c)) {
+				if (!map.get(c).equals(words[i])) {
 					return false;
 				}
-				continue;
+			} else {
+				map.put(c, words[i]);
 			}
-			map.put(pattern.charAt(i), strArr[i]);
 		}
-		
 		return true;
 	}
 }
