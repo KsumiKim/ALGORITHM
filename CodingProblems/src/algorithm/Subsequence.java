fc3cd9a7485e8104ad46d3aa440812df9c7794b1
KsumiKim <ksumiKim@gmail.com>
2022-03-07 22:33:06 +0900

    return true if s is a subsequence of t

--- CodingProblems/src/algorithm/Subsequence.java
+++ CodingProblems/src/algorithm/Subsequence.java
@@ -1,7 +1,5 @@
 package algorithm;
 
-import java.util.Stack;
-
 public class Subsequence {
 
 	public static void main(String[] args) {
@@ -20,8 +18,13 @@ public class Subsequence {
 		String s5 = "b";
 		String t5 = "abc";
 		
+		String s6 = "aaaaaa";
+		String t6 = "bbaaaa";
 		
-		System.out.println(isSubsequence2(s5, t5));
+		String s7 = "ab";
+		String t7 = "baab";
+		
+		System.out.println(isSubsequence2(s7, t7));
 	}
 	
 	// method telling if s is a subsequence of t
@@ -46,25 +49,17 @@ public class Subsequence {
 		return false;
 	}
 
-	
 	public static boolean isSubsequence2(String s, String t) {
-		if (s.isEmpty())
-			return true;
-		
-		if (t.isEmpty())
-			return false;
-		
-		Stack<Character> stack = new Stack<>();
-		int sIdx = 0;
-		
-		for (int i = 0; i < t.length() && sIdx < s.length(); i++) {
+		int prevIdx = -1;
+		for (char c : s.toCharArray()) {
+			int currIdx = t.indexOf(c, prevIdx + 1);
 			
-			if (t.charAt(i) != s.charAt(sIdx)) 
-				continue;
-
-			stack.push(t.charAt(i));
-			sIdx++;
+			if (t.indexOf(c) < 0 || prevIdx > currIdx) {
+				return false;
+			}
+			prevIdx = currIdx;
 		}
-		return sIdx == s.length();
+		
+		return true;
 	}
 }
