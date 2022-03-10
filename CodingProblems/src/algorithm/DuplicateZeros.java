ec29a9d4b6b50b9f2e3b35aa93420d3ead3eaf11
KsumiKim <ksumiKim@gmail.com>
2022-03-10 21:47:57 +0900

    return then given array after duplicating zeroes in place

--- CodingProblems/src/algorithm/DuplicateZeros.java
+++ CodingProblems/src/algorithm/DuplicateZeros.java
@@ -10,7 +10,7 @@ public class DuplicateZeros {
 		int[] arr2 = {1, 5, 2, 0, 6, 8, 0, 6, 0};
 		
 		DuplicateZeros dZeros = new DuplicateZeros();
-		dZeros.duplicateZeros(arr2);
+		dZeros.duplicateZeros2(arr2);
 
 		for (int n : arr2)
 			System.out.print(n + ", ");
@@ -30,6 +30,24 @@ public class DuplicateZeros {
     		idx++;
     		if (num == 0 && idx + 1 <= arr.length) 
     			arr[idx++] = num;
+    	}
+    }
+
+    
+    public void duplicateZeros2(int[] arr) {
+    	
+    	for (int i = 0; i < arr.length; i++) {
+    		if (arr[i] != 0) {
+    			continue;
+    		}
+    		
+    		int prev = -1;
+    		for (int j = i + 1; j < arr.length; j++) {
+    			int curr = arr[j];
+    			arr[j] = prev < 0 ? 0 : prev;
+    			prev = curr;
+    		}
+    		i++;
     	}
     }
 }
