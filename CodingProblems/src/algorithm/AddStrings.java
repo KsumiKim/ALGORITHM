832860c95abdc7043229f4416ef54e3d1bc769e7
KsumiKim <ksumiKim@gmail.com>
2022-03-15 20:51:46 +0900

    return sum of string num1 and num2

--- CodingProblems/src/algorithm/AddStrings.java
+++ CodingProblems/src/algorithm/AddStrings.java
@@ -2,6 +2,14 @@ package algorithm;
 
 public class AddStrings {
 	
+	public static void main(String[] args) {
+		String num1 = "456";
+		String num2 = "77";
+		
+		String res = addStrings(num1, num2);
+		System.out.println(res);
+	}
+	
 	public String getSumOfTwoStrings(String num1, String num2) {
 		StringBuffer sb = new StringBuffer();
 		int carry = 0;
@@ -23,4 +31,60 @@ public class AddStrings {
 		
 		return sb.reverse().toString();
 	}
+
+
+    public static String addStrings(String num1, String num2) {
+    	StringBuilder sb = new StringBuilder();
+    	
+    	int i = num1.length() - 1;
+    	int j = num2.length() - 1;
+    	int increment = 0;
+    	
+    	while (i >= 0 && j >= 0) {
+    		int digit1 = num1.charAt(i) - 48;
+    		int digit2 = num2.charAt(j) - 48;
+    		
+    		int sum = digit1 + digit2 + increment;
+    		increment = 0;
+    		
+    		if (sum > 10) {
+    			increment++;
+    			sb.append(sum % 10);
+    		} else {
+    			sb.append(sum);
+    		}
+    		i--;
+    		j--;
+    	}
+    	
+    	while (i >= 0) {
+    		int digit1 = num1.charAt(i) - 48;
+    		int sum = digit1 + increment;
+    		increment = 0;
+    		
+    		if (sum > 10) {
+    			increment++;
+    			sb.append(sum / 10);
+    		} else {
+    			sb.append(sum);
+    		}
+    		i--;
+    	}
+    	
+    	while (j >= 0) {
+    		int digit2 = num2.charAt(j) - 48;
+    		int sum = digit2 + increment;
+    		increment = 0;
+    		
+    		if (sum > 10) {
+    			increment++;
+    			sb.append(sum / 10);
+    		} else {
+    			sb.append(sum);
+    		}
+    		j--;
+    	}
+    	
+    	return sb.reverse().toString();
+    }
 }
