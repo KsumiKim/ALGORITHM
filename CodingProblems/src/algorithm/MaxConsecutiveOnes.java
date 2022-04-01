d8130b0df370b3fe93308da72ecedd8389fed3fb
KsumiKim <ksumiKim@gmail.com>
2022-04-01 22:03:06 +0900

    return the max consecutive number of ones in the given array

--- CodingProblems/src/algorithm/MaxConsecutiveOnes.java
+++ CodingProblems/src/algorithm/MaxConsecutiveOnes.java
@@ -5,7 +5,8 @@ public class MaxConsecutiveOnes {
 	public static void main(String[] args) {
 		MaxConsecutiveOnes maxOnes = new MaxConsecutiveOnes();
 		int[] nums = {1, 1, 0, 1, 1, 1};
-		int result = maxOnes.getMaxConsOnes(nums);
+		int[] nums2 = {1, 0, 1, 1, 0, 1};
+		int result = maxOnes.findMaxConsecutiveOnes(nums2);
 		
 		System.out.println(result);
 		
@@ -21,4 +22,20 @@ public class MaxConsecutiveOnes {
 		
 		return max;
 	}
+
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+    	int res = 0, max = 0;
+    	
+    	for (int i = 0; i < nums.length; i++) {
+    		if (nums[i] != 1) {
+    			res = Math.max(res, max);
+    			max = 0;
+    			continue;
+    		}
+    		max++;
+    	}
+    	
+    	return Math.max(res, max);
+    }
 }
