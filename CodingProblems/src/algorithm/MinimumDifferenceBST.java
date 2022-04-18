6baa9092d39a6a07c330be0d47180326ed7fac3f
KsumiKim <ksumiKim@gmail.com>
2022-04-18 20:22:36 +0900

    return minimum difference between nodes of given BST

--- CodingProblems/src/algorithm/MinimumDifferenceBST.java
+++ CodingProblems/src/algorithm/MinimumDifferenceBST.java
@@ -6,15 +6,21 @@ public class MinimumDifferenceBST {
 		TreeNode root = new TreeNode(4);
 		root.left = new TreeNode(2);
 		root.right = new TreeNode(6);
-		root.left.left = new TreeNode(-1);
+		root.left.left = new TreeNode(1);
 		root.left.right = new TreeNode(3);
 		
+		TreeNode root2 = new TreeNode(1);
+		root2.left = new TreeNode(0);
+		root2.right = new TreeNode(48);
+		root2.right.left = new TreeNode(12);
+		root2.right.right = new TreeNode(49);
+		
 		MinimumDifferenceBST minDiff = new MinimumDifferenceBST();
-		int res = minDiff.minDiffInBST(root);
+		int res = minDiff.minDiffInBST2(root);
 		System.out.println(res);
 	}
 	
-	boolean init = false;
+	boolean init = true;
 	int min = Integer.MAX_VALUE;
 	int prev = 0;
 	
@@ -36,4 +42,24 @@ public class MinimumDifferenceBST {
 		}
 		inorder(node.right);
 	}
+
+    public int minDiffInBST2(TreeNode root) {
+    	inorder2(root);
+    	return min;
+    }
+    
+    private void inorder2(TreeNode node) {
+    	if (node == null) {
+    		return;
+    	}
+    	
+    	inorder2(node.left);
+    	if (init) {
+    		init = false;
+    	} else {
+    		min = Math.min(min, node.val - prev);
+    	}
+		prev = node.val;
+    	inorder2(node.right);
+    }
 }
