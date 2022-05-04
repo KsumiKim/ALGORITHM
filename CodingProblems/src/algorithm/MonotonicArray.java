9e5a3e4a40315597d9b0e12763e78bdb7b9c5732
KsumiKim <ksumiKim@gmail.com>
2022-05-04 19:32:33 +0900

    return if the given array is monotonic (either increasing or decreasing)

--- CodingProblems/src/algorithm/MonotonicArray.java
+++ CodingProblems/src/algorithm/MonotonicArray.java
@@ -4,30 +4,37 @@ public class MonotonicArray {
 
 	public static void main(String[] args) {
 		int[] A = {1, 2, 4, 5};
+		int[] A2 = {1, 2, 2, 3};
+		int[] A3 = {6, 5, 4, 4};
+		int[] A4 = {1, 3, 2};
+		
 		MonotonicArray mArray = new MonotonicArray();
-		boolean res = mArray.isMonotonic(A);
+		boolean res = mArray.isMonotonic(A3);
 		System.out.println(res);
 	}
 	
 	// An array is monotonic if it is either monotone increasing or decreasing
-    public boolean isMonotonic(int[] A) {
+    public boolean isMonotonic(int[] nums) {
+    	if (nums.length == 1) {
+    		return true;
+    	}
     	
-    	boolean isIncreasing = true;
+    	boolean increasing = true, decreasing = true;
     	
-    	if (A[0] > A[A.length - 1])
-    		isIncreasing = false;
-    	
-    	for (int i = 0; i < A.length - 1; i++) {
+    	for (int i = 0; i < nums.length - 1; i++) {
     		
-    		if (isIncreasing) {
-    			if (A[i] > A[i + 1])
-    			return false;
-    		} else {
-    			if (A[i] < A[i + 1])
+    		if (increasing && decreasing) {
+        		if (nums[i] < nums[i + 1]) {
+        			decreasing = false;
+        		} else if (nums[i] > nums[i + 1]) {
+        			increasing = false;
+        		}
+    		}
+    		
+    		if ((increasing && nums[i] > nums[i + 1]) || (decreasing && nums[i] < nums[i + 1])) {
     			return false;
     		}
     	}
-
     	return true;
     }
 }
