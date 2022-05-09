b607ee2037db201362913d5c69f15120ea5bcdcc
KsumiKim <ksumiKim@gmail.com>
2022-05-09 20:46:46 +0900

    return all root-to-leaf paths of the given BST

--- CodingProblems/src/algorithm/BinaryTreePaths.java
+++ CodingProblems/src/algorithm/BinaryTreePaths.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 public class BinaryTreePaths {
 	
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.left.right = new TreeNode(3);
+		root.right = new TreeNode(5);
+		
+		List<String> list = bstPath(root);
+		System.out.println(list);
+	}
+	
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> pathList = new ArrayList<>();
         
@@ -34,6 +44,30 @@ public class BinaryTreePaths {
     			sb.append("->");
     		helper(node.right, sb.toString(), list);
     	}
+    }
+    
+    public static List<String> bstPath(TreeNode root) {
+    	List<String> list = new ArrayList<>();
+    	traverse(root, new StringBuilder(), list);
+    	return list;
+    }
+    
+    private static void traverse(TreeNode node, StringBuilder path, List<String> list) {
+    	if (node == null) {
+    		return;
+    	}
+    	
+    	int initLength = path.length();
+    	path.append(node.val);
+    	
+    	if (node.left == null && node.right == null) {
+    		list.add(path.toString());
+    	} else {
+    		path.append("->");
+    		traverse(node.left, path, list);
+    		traverse(node.right, path, list);
+    	}
+    	path.setLength(initLength);
     }
 }
 
