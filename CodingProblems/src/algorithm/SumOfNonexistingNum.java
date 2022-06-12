7f7a7e8ac83c2e9016ecb5295d988213fe414051
KsumiKim <ksumiKim@gmail.com>
2022-06-13 00:03:40 +0900

    return sum of all non-existing numbers

--- CodingProblems/src/algorithm/SumOfNonexistingNum.java
+++ CodingProblems/src/algorithm/SumOfNonexistingNum.java
@@ -12,7 +12,7 @@ public class SumOfNonexistingNum {
         
         for (int i = 0; i < arr.length; i++) {
         	if (arr[i] == 0) {
-        		answer = arr[i];
+        		answer = i;
         	}
         }
         return answer;
